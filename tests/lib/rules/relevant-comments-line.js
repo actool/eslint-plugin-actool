b9faa6d71aa61b942669045c5f213bb22c24b7b2
Ilya Azin <martis.azin@gmail.com>
2020-11-24 16:00:54 +0300

    chore: update fixture-code in tests
    #25

--- tests/lib/rules/relevant-comments-line.js
+++ tests/lib/rules/relevant-comments-line.js
@@ -18,6 +18,37 @@ const rule = require("../../../lib/rules/relevant-comments-line"),
 //------------------------------------------------------------------------------
 
 const defaultCode = "console.log('...')";
+// TODO: get code by filename later
+const relevantCommentsCode = `
+type DictionariesAPI = {
+    getSchedules: () => any;
+    getVehicles: () => any;
+    getIssueStatuses: () => any;
+}
+
+// TODO: fix types
+const API = {} as DictionariesAPI;
+
+/**
+ * Get schedules from server
+ */
+const getSharedDictionaries = async () => {
+    const { data: schedules } = await API.getSchedules();
+    const { data: vehicles } = await API.getVehicles();
+    const { data: issueStatuses } = await API.getIssueStatuses();
+
+    return {
+        schedules,
+        vehicles,
+        issueStatuses,
+    }
+}
+
+/**
+ * TODO: simplify fetching
+ * FIXME: types
+ */
+`
 
 /**
  * @type {import("eslint").RuleTester}
@@ -25,13 +56,17 @@ const defaultCode = "console.log('...')";
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 }});
 ruleTester.run("relevant-comments-line", rule, {
     valid: [
+        // {
+        //     code: defaultCode,
+        //     filename: "lib/rules/max-tags-file.js",
+        // },
+        // {
+        //     code: defaultCode,
+        //     filename: "docs/rules/max-tags-file.md",
+        // },
         {
-            code: defaultCode,
-            filename: "lib/rules/max-tags-file.js",
-        },
-        {
-            code: defaultCode,
-            filename: "docs/rules/max-tags-file.md",
+            code: relevantCommentsCode,
+            filename: "tests/lib/fixtures/relevant-comments.ts",
         },
         // {
         //     code: defaultCode,
