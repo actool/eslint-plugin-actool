0f80e514f002f9bbca9764cbfd3ffaeae19360c9
Ilya Azin <martis.azin@gmail.com>
2020-12-07 03:18:39 +0300

    chore(lint): add base inner linting with eslint + prettier
    #75

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+// TODO: add @typescript-eslint
+
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2020,
+        ecmaFeatures: {
+            jsx: true,
+            modules: true,
+        },
+        sourceType: "module",
+    },
+    env: {
+        browser: true,
+        es6: true,
+        amd: true,
+        node: true,
+    },
+    plugins: [],
+    extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
+    rules: {
+        // variables
+        "prefer-const": 2,
+        "no-var": 2,
+        // base
+        "camelcase": [1, { ignoreDestructuring: true, ignoreImports: true, properties: "never" }],
+        "no-else-return": 2,
+        "max-len": [1, { code: 120 }],
+        "dot-notation": 2,
+        "eol-last": 2,
+        // alert, console
+        "no-alert": 2,
+        "no-console": 0,
+        // equals
+        "eqeqeq": 1,
+        "no-eq-null": 2,
+        // function
+        "max-params": [1, 2],
+        "max-lines-per-function": [1, 48],
+        "arrow-parens": [2, "always"],
+        // TODO: add other plugins (unicorn, ...)
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    singleQuote: false,
+    semi: true,
+    trailingComma: "all",
+    arrowParens: "always",
+    quoteProps: "consistent",
+    endOfLine: "auto",
+    overrides: [
+        {
+            files: "*.{json,yml,md}",
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
