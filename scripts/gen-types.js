a6c6cd3358047398f532520ec3d8b997c4b57457
Ilya Azin <martis.azin@gmail.com>
2020-12-07 22:05:13 +0300

    chore(types): add rule types generating (by schema)
    #83

---
+++ scripts/gen-types.js
@@ -0,0 +1,54 @@
+const { compile } = require("json-schema-to-typescript");
+const { rules } = require("../lib");
+const { writeFileSync } = require("fs");
+
+/**
+ * Settings
+ */
+const SCRIPT_LABEL = "[🛠️  gen-types] ";
+const PAD = 24;
+/**
+ * Logs helpers
+ */
+/** @param {string} message */
+const print = (message) => process.stdout.write(message);
+const println = (message) => print(`${message}\n`);
+const withLabel = (message) => `${SCRIPT_LABEL}${message}`;
+
+/**
+ * JSDoc types generating
+ */
+async function main() {
+    println(withLabel("Starting..."));
+    const ruleNames = Object.keys(rules);
+    for (const ruleName of ruleNames) {
+        print(withLabel(`> ${ruleName.padEnd(PAD, " ")}`));
+        await generateTypesForRule(ruleName);
+    }
+}
+
+/**
+ * Generate types for specific rule
+ * @param {string} ruleName
+ */
+async function generateTypesForRule(ruleName) {
+    /** @type {import("eslint").Rule.RuleModule} */
+    const { meta } = rules[ruleName];
+    const { schema } = meta;
+
+    if (!schema || !schema.length) {
+        println("❌ (not schema provided)");
+        return;
+    }
+
+    await compile(schema[0], `${ruleName}Options`)
+        .then((genTypes) => {
+            writeFileSync("lib/rules/types.d.ts", genTypes);
+            println("✔️");
+        })
+        .catch(() => {
+            println(`❌ (compilation error)`);
+        });
+}
+
+main();
